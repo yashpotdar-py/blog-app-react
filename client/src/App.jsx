c4563dd70ac401c5b3d245f9589dfe8b8388f3c8
Yash Potdar <113132066+yashpotdar-py@users.noreply.github.com>
2024-03-22 12:59:29 +0530

    Creating the Header Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,12 @@ import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
+import Header from "./components/Header/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
