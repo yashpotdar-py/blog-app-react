7b36f438aa0b608a8e29f8d4e45c25f4126db754
Yash Potdar <113132066+yashpotdar-py@users.noreply.github.com>
2024-03-23 14:35:32 +0530

    Creating and Connecting MongoDB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDB Connected.");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 const port = 3000;
