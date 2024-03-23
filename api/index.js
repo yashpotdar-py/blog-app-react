7036865ac437e743943c4f7c221962d81336f76e
Yash Potdar <113132066+yashpotdar-py@users.noreply.github.com>
2024-03-23 22:35:14 +0530

    Creating API route for Sign Up

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -17,8 +18,11 @@ mongoose
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
 app.use("/api/user/", userRoutes);
+app.use("/api/auth", authRoutes);
