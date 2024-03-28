0b9d0e50ffe9eae5eba6b0cf21048c9820d3366b
Yash Potdar <113132066+yashpotdar-py@users.noreply.github.com>
2024-03-28 09:39:34 +0530

    Adding functionality to Sign In Page

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,124 @@
-import React from 'react'
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Logo from "../components/Logo/Logo";
 
 export default function SignIn() {
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({});
+
+  const handleChange = (event) => {
+    const { id, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [id]: value.trim(),
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!formData.email || !formData.password) {
+      return setErrorMessage("Please fill out all the fields.");
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if (res.ok) {
+        navigate("/");
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
-    <div>SignIn</div>
-  )
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-10">
+        {/* Left Side */}
+        <div className="flex-1">
+          <Logo />
+          <p className="text-sm mt-5">
+            This a demo project. You can sign in with your email and password or
+            with Gmail.
+          </p>
+        </div>
+        {/* Right Side */}
+        <div className="flex-1">
+          <form
+            method="post"
+            className="flex flex-col gap-4"
+            onSubmit={handleSubmit}
+          >
+            {/* <div>
+              <Label value="Enter Username" />
+              <TextInput
+                type="text"
+                placeholder="Username"
+                id="username"
+                onChange={handleChange}
+              />
+            </div> */}
+            <div>
+              <Label value="Enter Email" />
+              <TextInput
+                type="email"
+                placeholder="youremail@example.com"
+                id="email"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value="Enter Password" />
+              <TextInput
+                type="password"
+                placeholder="Password"
+                id="password"
+                hidden
+                onChange={handleChange}
+              />
+            </div>
+            <Button
+              gradientDuoTone={"purpleToBlue"}
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size={"sm"} />
+                  <span className="pl-3">Loading</span>
+                </>
+              ) : (
+                "Sign In"
+              )}
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Don't have an Account?</span>
+            <Link to={"/sign-up"} className="text-blue-500">
+              Sign Up
+            </Link>
+          </div>
+          {errorMessage && (
+            <Alert className="mt-5" color={"failure"}>
+              {errorMessage}
+            </Alert>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
