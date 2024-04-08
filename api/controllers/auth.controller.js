28c57951523e639984488ab9e7df1ad69e39801c
Yash Potdar <113132066+yashpotdar-py@users.noreply.github.com>
2024-04-08 19:09:59 +0530

    Adding isAdmin paramter to Schema

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -53,7 +53,7 @@ export const signin = async (req, res, next) => {
     if (!validPassword) {
       return next(errorHandler(400, "Invalid Credentials."));
     }
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET_KEY);
+    const token = jwt.sign({ id: validUser._id, isAdmin: validUser.isAdmin }, process.env.JWT_SECRET_KEY);
 
     const { password: pass, ...rest } = validUser._doc;
 
@@ -73,7 +73,7 @@ export const google = async (req, res, next) => {
   try {
     const user = await User.findOne({ email });
     if (user) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY);
+      const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET_KEY);
       const { password: pass, ...rest } = user._doc;
       res
         .status(200)
@@ -96,7 +96,7 @@ export const google = async (req, res, next) => {
         profilePicture: googlePhotoUrl,
       });
       await newUser.save();
-      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET_KEY);
+      const token = jwt.sign({ id: newUser._id, isAdmin: newUser.isAdmin }, process.env.JWT_SECRET_KEY);
       const { password: pass, ...rest } = newUser._doc;
       res
         .status(200)
